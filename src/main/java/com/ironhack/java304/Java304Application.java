bfb8a788466b2928a2505c6d6dc079b2e9a81081
Sara El Badri <saraelbadri.ci@gmail.com>
2023-02-23 14:46:57 +0100

    first

---
+++ src/main/java/com/ironhack/java304/Java304Application.java
@@ -0,0 +1,13 @@
+package com.ironhack.java304;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java304Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java304Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/java304/Java304ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.java304;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java304ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
